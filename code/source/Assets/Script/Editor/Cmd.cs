28e500106e6070da73fd61adf8b43740cff615ce
user <user@062f0958-56b8-ac45-908c-11283a37e8c9>
2018-01-22 03:11:33 +0000

    SvnVersion
    git-svn-id: file:///E/SVN/yg/trunk@6 062f0958-56b8-ac45-908c-11283a37e8c9

---
+++ code/source/Assets/Script/Editor/Cmd.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+using Wf;
+
+public class Cmd : Editor
+{
+    [MenuItem("Tools/MakeZipFile")]
+    public static void MakeZipFile()
+    {
+        Debug.LogError(DecompressUtils.MakeZipFile("E:\\Git\\yg\\trunk\\潘之琳"));
+    }
+
+    [MenuItem("Tools/UnMakeZipFile")]
+    public static void UnMakeZipFile()
+    {
+        Debug.LogError(DecompressUtils.UnMakeZipFile("E:\\Git\\yg\\trunk\\潘之琳3.zip"));
+    }
+}

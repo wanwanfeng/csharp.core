123a61701053751590e5d97b00083d1c08966660
user <user@062f0958-56b8-ac45-908c-11283a37e8c9>
2018-04-26 13:21:17 +0000

    fix
    git-svn-id: file:///E/SVN/yg/trunk@132 062f0958-56b8-ac45-908c-11283a37e8c9

--- code/tools/test/Script/CreateExcelCell.cs
+++ code/tools/test/Script/CreateExcelCell.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Library.Helper;
 
 namespace Script
 {
@@ -18,14 +19,15 @@ namespace Script
             //root = "D:/Work/yuege/res/app/assets/app/daifanyi/";
             //root = "D:/Work/yuege/res/app/assets/app/daifanyi/image_new/";
 
-            root = @"D:\Work\mfxy\ron_mfsn2\banshu\madomagi_native\Resources\package".Replace("\\", "/");
+            {
+                //root = @"D:\Work\mfxy\ron_mfsn2\banshu\madomagi_native\Resources\package".Replace("\\", "/");
+                var newarray = ReadAllLines("new");
+                var oldarray = ReadAllLines("old");
+                var res = newarray.Except(oldarray).Where(p => !string.IsNullOrEmpty(p)).Select(p => root + p).ToList();
+                res.Sort();
+                RunList(res);
+            }
 
-
-            var oldarray = ReadAllLines("old");
-            var newarray = ReadAllLines("new");
-            var res = newarray.Except(oldarray).Where(p => !string.IsNullOrEmpty(p)).Select(p => root + p).ToList();
-            res.Sort();
-            RunList(res);
             WriteAllLines(dic);
         }
 
@@ -34,10 +36,13 @@ namespace Script
             try
             {
                 dic[re.Replace(root, "")] = GetExcelCell(re);
+                var newName = @"D:\Work\mfxy\资料\小圆\xy\mfxy\res" + re.Replace(root, "");
+                FileHelper.CreateDirectory(newName);
+                File.Copy(re, newName);
             }
-            catch (Exception)
+            catch (Exception e)
             {
-                Console.WriteLine(re);
+                Console.WriteLine(re + "\t" + e.Message);
             }
         }
     }

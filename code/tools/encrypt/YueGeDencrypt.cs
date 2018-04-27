a4eb836dd6b1b6cb51154e083991a35fa23fd92b
xianfeng <1239117205@qq.com>
2018-04-27 17:12:35 +0800

    月歌资源加解密工具

---
+++ code/tools/encrypt/YueGeDencrypt.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Library.Helper;
+
+namespace encrypt
+{
+    internal class YueGeDencrypt
+    {
+
+        public List<string> extensionList = new List<string>()
+        {
+            ".acb"
+        };
+
+        private string Md5Key = "kEfGhnNmeu4YYuhv";
+
+        private string cmd;
+
+        public YueGeDencrypt()
+        {
+            Dictionary<string, string> cache = GetCache(Md5Key);
+
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.Write("输入加密的目标目录:");
+
+            cmd = Console.ReadLine() ?? "e";
+            if (cmd == "e")
+                Environment.Exit(0);
+
+            string root = cmd;
+            if (Directory.Exists(root))
+            {
+                var folder = Path.GetFileName(root);
+                root += root.EndsWith("\\") ? "" : "\\";
+                var files =
+                    Directory.GetFiles(root, "*.*", SearchOption.AllDirectories)
+                        .Select(p => p.Replace(root, "").Replace("\\", "/"))
+                        .Where(p => Path.GetFileNameWithoutExtension(p).Length == 32)
+                        .ToArray();
+
+                var index = 0;
+
+                var dirRoot = root.Replace(folder, "");
+                foreach (string file in files)
+                {
+                    var path = folder + "/" + file;
+
+                    var newPath = "";
+                    if (cache.TryGetValue(path, out newPath))
+                    {
+                        var outPath = dirRoot + newPath;
+                        FileHelper.CreateDirectory(outPath);
+                        var p = ((float) (index++)/files.Length).ToString("P") + "\t" + path;
+                        if (extensionList.Contains(Path.GetExtension(path)))
+                        {
+                            Console.WriteLine("路径解码中..." + p);
+                            File.Copy(root + file, outPath, true);
+                        }
+                        else
+                        {
+                            Console.WriteLine("路径解码并文件解密中..." + p);
+                            File.WriteAllBytes(outPath, UtilSecurity.DecryptionBytes(File.ReadAllBytes(root + file)));
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("不存在的资源文件！");
+                    }
+                }
+            }
+            Console.WriteLine("按任意键结束!");
+            Console.ReadKey();
+        }
+
+        private Dictionary<string, string> GetCache(string md5Key)
+        {
+            Dictionary<string, string> cache = new Dictionary<string, string>();
+
+            Console.Write("输入原始目录获取md5对应关系:");
+            cmd = Console.ReadLine() ?? "e";
+            if (cmd == "e")
+                Environment.Exit(0);
+
+            string root = cmd;
+            if (!Directory.Exists(root)) return cache;
+
+            var folder = Path.GetFileName(root);
+            if (folder != "app")
+            {
+                Console.WriteLine("文件夹选择错误，必须以/app结尾");
+                return cache;
+            }
+
+            root += root.EndsWith("\\") ? "" : "\\";
+            var files =
+                Directory.GetFiles(root, "*.*", SearchOption.AllDirectories)
+                    .Select(p => p.Replace(root, "").Replace("\\", "/"))
+                    .Where(p => Path.GetFileNameWithoutExtension(p).Length != 32)
+                    .ToArray();
+
+
+            var index = 0;
+            foreach (string file in files)
+            {
+                Console.WriteLine("路径关系获取中..." + ((float) (index++)/files.Length).ToString("P") + "\t" + file);
+                var path = file;
+                var dirName = Path.GetDirectoryName(path).Replace("\\", "/");
+                var fileName = Path.GetFileName(path);
+                var newPath = new Library.MD5().Encrypt(dirName + md5Key) + "/" +
+                              new Library.MD5().Encrypt(fileName + md5Key);
+                cache[newPath] = file;
+            }
+
+            return cache;
+        }
+    }
+}
---
+++ code/tools/encrypt/test.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace encrypt
+{
+    public class test
+    {
+        public static void GetValue(string[] args)
+        {
+            Queue<string> queue = new Queue<string>(args);
+            string type = queue.Count == 0 ? "" : queue.Dequeue();
+            if (string.IsNullOrEmpty(type))
+                return;
+
+            string res = "";
+            switch (type)
+            {
+                case "md5":
+                {
+                    string input = queue.Count == 0 ? "" : queue.Dequeue();
+                    res = Library.Encrypt.MD5(input);
+                }
+                    break;
+                case "aes":
+                {
+                    string input = queue.Count == 0 ? "" : queue.Dequeue();
+                    string key = queue.Count == 0 ? "" : queue.Dequeue();
+                    string third = queue.Count == 0 ? "-e" : queue.Dequeue();
+                    Library.AES.Key = key;
+                    var resBytes = third == "-d"
+                        ? Library.Dencrypt.AES(File.ReadAllText(input))
+                        : Library.Encrypt.AES(File.ReadAllText(input));
+                    File.WriteAllText(input, resBytes);
+                }
+                    break;
+                case "help":
+                {
+                    res = @"
+输入格式（类型，源串，密钥，其他）
+MD5加密（string:md5,string:input,string:[key],string[-h|-e]）
+AES加密（string:aes,string:input,string:[key],string[-e]）
+AES解密（string:aes,string:input,string:[key],string[-d]）
+";
+                }
+                    break;
+            }
+            Console.WriteLine(res);
+        }
+    }
+}

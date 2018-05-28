2759ef1fff1dde37188b9344b24825901ef30c14
user <user@062f0958-56b8-ac45-908c-11283a37e8c9>
2018-05-28 03:07:52 +0000

    svnVersion
    git-svn-id: file:///E/SVN/yg/trunk@161 062f0958-56b8-ac45-908c-11283a37e8c9

--- code/tools/svnVersion/Common/CmdHelp.cs
+++ code/tools/svnVersion/Common/CmdHelp.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Library.Extensions;
 
 namespace SvnVersion
 {
@@ -68,50 +69,52 @@ namespace SvnVersion
             process.Close();
         }
 
-        public virtual string[] RunCmd(string input, bool isFile = false)
-        {
-            List<string> res = new List<string>();
-            Console.WriteLine(input);
-            process.Start(); //启动程序
-            //向cmd窗口发送输入信息
-            if (isFile)
-            {
-                var temp = DateTime.UtcNow.ToString("yyyy-MM-dd-hh-mm-ss") + ".txt";
-                process.StandardInput.WriteLine(input + ">" + temp + "&exit");
-                process.StandardInput.AutoFlush = true;
-                process.WaitForExit();
-                res = File.ReadAllLines(temp).ToList();
-                File.Delete(temp);
-                if (res.Count == 0)
-                    res.Add("");
-                return res.ToArray();
-            }
 
+        public virtual string[] CmdReadAll(string input)
+        {
+            process.Start(); //启动程序
             process.StandardInput.WriteLine(input + "&exit");
             process.StandardInput.AutoFlush = true;
-            //p.StandardInput.WriteLine("exit");
-            //向标准输入写入要执行的命令。这里使用&是批处理命令的符号，表示前面一个命令不管是否执行成功都执行后面(exit)命令，如果不执行exit命令，后面调用ReadToEnd()方法会假死
-            //同类的符号还有&&和||前者表示必须前一个命令执行成功才会执行后面的命令，后者表示必须前一个命令执行失败才会执行后面的命令
+
+            string output = process.StandardOutput.ReadToEnd();
+            var res = new List<string>(output.AsStringArray('\r', '\n'));
+
             process.WaitForExit();
-            //获取cmd窗口的输出信息
 
-            //string output = process.StandardOutput.ReadToEnd();
-            //List<string> res = output.Split('\r','\n').Where(p=>!string.IsNullOrEmpty(p)).ToList();
-            //Console.WriteLine("");
-            //Console.WriteLine(res[2].Replace("&exit", ""));
-            //return res.Skip(3).ToArray();
+            res = res.Where(p => !string.IsNullOrEmpty(p)).ToList();
+            Console.WriteLine(res[2].Replace("&exit", ""));
+            res = res.Skip(3).ToList();
+            return res.ToArray();
+        }
 
+        public virtual string[] CmdReadLine(string input)
+        {
+            process.Start(); //启动程序
+            process.StandardInput.WriteLine(input + "&exit");
+            process.StandardInput.AutoFlush = true;
+
+            var res = new List<string>();
             StreamReader reader = process.StandardOutput;
-            res.Add(reader.ReadLine());
             while (!reader.EndOfStream)
             {
                 res.Add(reader.ReadLine());
             }
-            Console.WriteLine("");
-            Console.WriteLine(res[3].Replace("&exit", ""));
-            if (res.Count == 4)
-                res.Add("");
-            return res.Skip(4).ToArray();
+
+            process.WaitForExit();
+
+            res = res.Where(p => !string.IsNullOrEmpty(p)).ToList();
+            Console.WriteLine(res[2].Replace("&exit", ""));
+            res = res.Skip(3).ToList();
+            return res.ToArray();
         }
+
+
+        public void PrintPercentage(int finishedCount, int totalCount)
+        {
+            decimal finishedPercentage = Convert.ToDecimal(finishedCount) / Convert.ToDecimal(totalCount);
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+            Console.WriteLine((finishedPercentage * 100).ToString("f1") + "%");
+        } 
+
     }
 }

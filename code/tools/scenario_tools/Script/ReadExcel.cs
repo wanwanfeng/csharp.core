f0bb6224274cee4908dd06752fb324071683ae32
xianfeng <1239117205@qq.com>
2018-06-06 11:34:17 +0800

    scenario_tools 输入输出调整

--- code/tools/scenario_tools/Script/ReadExcel.cs
+++ code/tools/scenario_tools/Script/ReadExcel.cs
@@ -16,14 +16,14 @@ namespace scenario_tools
 
         public ReadExcel()
         {
-            path = SystemExtensions.GetInputStr("请输入剧情目录（.xls）：", "", @"D:\Work\yuege\www\assets\res\scenario.xls");
-            var dic = new ExcelByReader().ReadFromExcels(path);
+            path = SystemExtensions.GetInputStr("请输入剧情文件（.xls|.xlsx）：", "", @"D:\Work\yuege\www\assets\res\scenario.xls");
+            var dic = new ExcelByNpoi().ReadFromExcels(path);
 
             Dictionary<string, List<JsonData>> dicT = new Dictionary<string, List<JsonData>>();
 
             foreach (KeyValuePair<string, List<List<object>>> keyValuePair in dic)
             {
-                JsonData jsonData = ExcelByReader.ConvertListToJson(keyValuePair);
+                JsonData jsonData = ExcelByBase.ConvertListToJson(keyValuePair);
 
                 foreach (JsonData json in jsonData)
                 {
@@ -41,10 +41,11 @@ namespace scenario_tools
                     }
                 }
             }
-
+            var index = 0;
             foreach (KeyValuePair<string, List<JsonData>> keyValuePair in dicT)
             {
                 var res = new JsonData();
+                Console.WriteLine(string.Format("剧情文件({1})：{0}", keyValuePair.Key, (++index)/(float) dicT.Count));
                 foreach (JsonData jsonData in keyValuePair.Value)
                 {
                     var xx = new JsonData();

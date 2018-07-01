62d7ca3e76b7ebac6a27a854f1f4eb59f8372ab4
liuxianfeng <1239117205@qq.com>
2018-07-01 22:09:13 +0800

    判断文件存在与否

--- code/tools/webUtils/Program.cs
+++ code/tools/webUtils/Program.cs
@@ -10,11 +10,12 @@ namespace webUtils
         public static void Main(string[] args)
         {
             string projectPath = args.FirstOrDefault();
+            string filter = args.Length <= 1 ? ".js,.html,.css" : args[1];
             if (Directory.Exists(projectPath))
             {
                 var list =
                     Directory.GetFiles(projectPath, "*.*", SearchOption.AllDirectories)
-                        .Where(p => ".js|.html|.css".Contains(Path.GetExtension(p)))
+                        .Where(p => filter.Contains(Path.GetExtension(p)))
                         .ToDictionary(p => p.Replace(projectPath + "\\", "").Replace("\\", "/"),
                             q => Md516(File.ReadAllBytes(q)))
                         .Select(p => string.Format("\"{0}\":\"{1}\"", p.Key, p.Value.ToLower()))
@@ -23,9 +24,13 @@ namespace webUtils
                 var content = string.Format("var fileTimeStamp = {{{0}}};", string.Join(",", list.ToArray()));
                 File.WriteAllText(projectPath + ".txt", content);
 
-                string[] re = File.ReadAllLines(projectPath + "/js/system/replacement.js");
-                re[0] = content;
-                File.WriteAllLines(projectPath + "/js/system/replacement.js", re);
+                var res = projectPath + "/js/system/replacement.js";
+                if (File.Exists(res))
+                {
+                    string[] re = File.ReadAllLines(res);
+                    re[0] = content;
+                    File.WriteAllLines(res, re);
+                }
             }
         }
 

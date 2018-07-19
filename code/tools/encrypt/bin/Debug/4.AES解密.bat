714a3be1f74463f9f62c10598e45186513ba31cb
liuxianfeng <1239117205@qq.com>
2018-07-19 19:33:10 +0800

    .gitignore

--- code/tools/encrypt/bin/Debug/4.AES解密.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-REM 密钥  "-e":加密 "-d":解密
-call tools\aes_encrypt.bat "kEfGenNmeu4BNEhv" "-d" "list.txt"
-pause
--- code/tools/encrypt/bin/Debug/test.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-setlocal enabledelayedexpansion
-( call encrypt.exe md5 image -e)>xx.txt
-pause

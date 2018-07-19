52f7b027a131c96784f43347b36de56771e5e042
liuxianfeng <1239117205@qq.com>
2018-07-20 02:27:31 +0800

    AttributeExtensions

--- code/tools/library/Library/Base/Extensions/AttributeExtensions.cs
+++ code/tools/library/Library/Base/Extensions/AttributeExtensions.cs
@@ -17,5 +17,41 @@ namespace Library.Extensions
         {
             return type.GetCustomAttributes(inherit).OfType<T>().FirstOrDefault();
         }
+
+        /// <summary>
+        /// 获取第一个自定义的Attribute
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="type"></param>
+        /// <param name="inherit"></param>
+        /// <returns></returns>
+        public static T GetCustomAttribute<T>(this MemberInfo type, bool inherit = false) where T : Attribute
+        {
+            return type.GetCustomAttributes(inherit).OfType<T>().FirstOrDefault();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="type"></param>
+        /// <param name="inherit"></param>
+        /// <returns></returns>
+        public static bool IsDefined<T>(this MemberInfo type, bool inherit = false) where T : Attribute
+        {
+            return type.IsDefined(typeof (T), inherit);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="target"></param>
+        /// <param name="inherit"></param>
+        /// <returns></returns>
+        public static bool IsDefined(this MemberInfo type, Type target, bool inherit = false)
+        {
+            return type.IsDefined(target, inherit);
+        }
     }
 }

2aacc4aec9a6b1878859938ea2035a1b1a013ca8
liuxianfeng <1239117205@qq.com>
2018-10-10 12:13:40 +0800

    命名空间调整 ObjectExtensions.cs Utils.cs SimpleLogger.cs

---
+++ code/tools/library/Library/Base/Extensions/ObjectExtensions.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Library.Extensions
+{
+    /// <summary>
+    /// Object扩展
+    /// </summary>
+    public static class ObjectExtensions
+    {
+        public static object GetPropertyValue(this object value, string name)
+        {
+            var info = value.GetType().GetProperty(name);
+            return info != null ? info.GetValue(value, null) : null;
+        }
+
+        public static object GetFieldValue(this object value, string name)
+        {
+            var info = value.GetType().GetField(name);
+            return info != null ? info.GetValue(value) : null;
+        }
+    }
+}

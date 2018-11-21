d7027472bc552dbd11d7bb6cfbb0de009c1d4bb2
liuxianfeng <1239117205@qq.com>
2018-11-21 14:35:00 +0800

    正则统一 RegexLanguaheEnum

--- code/tools/library-excel/Script/SystemExcel.cs
+++ code/tools/library-excel/Script/SystemExcel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using Library.Extensions;
 
@@ -7,6 +8,21 @@ namespace Library.Excel
 {
     public class BaseSystemExcel : BaseSystemConsole
     {
+        [Description("https://www.cnblogs.com/csguo/p/7401874.html")]
+        public enum RegexLanguaheEnum
+        {
+            [Description("日文"), StringValue("([\u0800-\u4E00])")]
+            日文 = 1,
+            [Description("日文平假名"), StringValue("([\u3040-\u309F])")]
+            日文平假名,
+            [Description("日文片假名"), StringValue("([\u30A0-\u30FF])")]
+            日文片假名,
+            [Description("日文片假名语音扩展"), StringValue("([\u31F0-\u31FF])")]
+            日文片假名语音扩展,
+            [Description("中文"), StringValue("([\u4E00-\u9FA5])")]
+            中文 = 10
+        }
+
         /// <summary>
         /// 已首列分组形成字典
         /// </summary>

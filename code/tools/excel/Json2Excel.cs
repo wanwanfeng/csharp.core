d0e90481d8cf8cb0d02379b50f8e0153eb7aab48
liuxianfeng <1239117205@qq.com>
2019-03-08 12:04:27 +0800

    bchong

---
+++ code/tools/excel/Json2Excel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using Library.Extensions;
+using Script;
+
+namespace Library.Excel
+{
+    public class Json2Excel
+    {
+        public enum CaoType
+        {
+            [Category("文件转换")] [Description("Excel->ToJson")] [TypeValue(typeof (ActionExcel.ToJson))] ExcelToJson,
+            [Category("文件转换")] [Description("Json->ToExcel")] [TypeValue(typeof (ActionJson.ToExcel))] JsonToExcel
+        }
+
+        private static void Main(string[] args)
+        {
+            SystemConsole.Run<CaoType>();
+        } 
+    }
+}

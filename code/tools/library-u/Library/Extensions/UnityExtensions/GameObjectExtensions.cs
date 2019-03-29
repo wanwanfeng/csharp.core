564cd4f3171ea3a79395ecca5faa900235a9a01b
liuxianfeng <1239117205@qq.com>
2019-03-29 19:35:41 +0800

    GameObjectExtensions.cs

--- code/tools/library-u/Library/Extensions/UnityExtensions/GameObjectExtensions.cs
+++ code/tools/library-u/Library/Extensions/UnityExtensions/GameObjectExtensions.cs
@@ -1,128 +1,44 @@
 ﻿using System;
-using System.Linq;
 using System.Collections.Generic;
-using Library.Extensions;
+using System.Linq;
 
 namespace UnityEngine.Library
 {
     /// <summary>
-    /// Unity扩展
+    /// GameObject扩展
     /// </summary>
-    public static class ExtensionsForUnity
+    public static class GameObjectExtensions
     {
-        /// <summary>
-        /// 转为Vector3型
-        /// </summary>
-        /// <param name="value"></param>
-        /// <param name="separator"></param>
-        /// <returns></returns>
-        public static Vector3 AsVector3(this string value, char separator = ',')
-        {
-            if (string.IsNullOrEmpty(value)) return Vector3.zero;
-            string[] result = value.Split(separator);
-            var temp = Array.ConvertAll(result, p => p.AsFloat());
-            switch (temp.Length)
-            {
-                default:
-                    return Vector3.zero;
-                case 1:
-                    return new Vector3(temp[0], 0, 0);
-                case 2:
-                    return new Vector3(temp[0], temp[1], 0);
-                case 3:
-                    return new Vector3(temp[0], temp[1], temp[2]);
-            }
-        }
 
         /// <summary>
-        /// 转为Vector2型
-        /// </summary>
-        /// <param name="value"></param>
-        /// <param name="separator"></param>
-        /// <returns></returns>
-        public static Vector2 AsVector2(this string value, char separator = ',')
-        {
-            if (string.IsNullOrEmpty(value)) return Vector2.zero;
-            var result = value.Split(separator);
-            var temp = Array.ConvertAll(result, p => p.AsFloat());
-            switch (temp.Length)
-            {
-                default:
-                    return Vector2.zero;
-                case 1:
-                    return new Vector2(temp[0], 0);
-                case 2:
-                    return new Vector2(temp[0], temp[1]);
-            }
-        }
-
-        /// <summary>
-        /// 转为string型
-        /// </summary>
-        /// <param name="value"></param>
-        /// <param name="separator"></param>
-        /// <returns></returns>
-        public static string AsString(this Vector3 value, char separator = ',')
-        {
-            return string.Format("{1}{0}{2}{0}{3}", separator, value.x, value.y, value.z);
-        }
-
-        /// <summary>
-        /// 转为string型
-        /// </summary>
-        /// <param name="value"></param>
-        /// <param name="separator"></param>
-        /// <returns></returns>
-        public static string AsString(this Vector2 value, char separator = ',')
-        {
-            return string.Format("{1}{0}{2}", separator, value.x, value.y);
-        }
-    }
-
-
-    public static class UnityExtensions
-    {
-        /// <summary>
-        ///  获取孩子GameObject列表
-        /// </summary>
-        /// <param name="target"></param>
-        /// <returns></returns>
-        public static List<GameObject> GetChildren(this GameObject target)
-        {
-            return target.transform.GetChildrenComponents<Transform>().Select(p => p.gameObject).ToList();
-        }
-
-        /// <summary>
-        /// 按路径获取物体
+        /// 获取组件(没有时自动添加)
         /// </summary>
         /// <param name="go"></param>
-        /// <param name="path"></param>
+        /// <param name="type"></param>
         /// <returns></returns>
-        public static GameObject GetGameObject(this GameObject go, string path)
+        public static Component GetOrAddComponent(this GameObject go, Type type)
         {
-            var trans = go.transform.Find(path);
-            if (trans)
-                return trans.gameObject;
-            Debug.LogWarning("Error! Can not find the gameObject.");
-            return null;
+            if (go == null) return null;
+            Component t = go.GetComponent(type);
+            if (t == null) t = go.AddComponent(type);
+            return t;
         }
 
         /// <summary>
-        /// 判断路径下组件是否存在
+        /// 获取组件(没有时自动添加)
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <param name="target"></param>
-        /// <param name="path"></param>
+        /// <param name="go"></param>
         /// <returns></returns>
-        public static bool ComponentIsNull<T>(this GameObject target, string path = null) where T : Component
+        public static T GetOrAddComponent<T>(this GameObject go) where T : Component
         {
-            var trans = target.transform;
-            if (!path.IsNullOrEmpty())
-                trans = target.transform.Find(path);
-            if (!trans) return false;
-            T t = trans.GetComponent<T>();
-            return t == null;
+            if (go == null)
+                return default(T);
+            T t = go.GetComponent<T>();
+            if (t == null) t = go.AddComponent<T>();
+            return t;
         }
+
 
         /// <summary>
         /// 查找组件,若果没有，则自动添加一个
@@ -137,29 +53,20 @@ namespace UnityEngine.Library
         }
 
         /// <summary>
-        /// 获取组件,若果没有，则自动添加一个
+        /// 判断路径下组件是否存在
         /// </summary>
+        /// <typeparam name="T"></typeparam>
         /// <param name="target"></param>
-        /// <param name="type"></param>
         /// <param name="path"></param>
         /// <returns></returns>
-        public static Component Find(this Component target, Type type, string path = null)
+        public static bool ComponentIsNull<T>(this GameObject target, string path = null) where T : Component
         {
             var trans = target.transform;
             if (!string.IsNullOrEmpty(path))
-            {
                 trans = target.transform.Find(path);
-            }
-            if (trans)
-            {
-                var component = trans.GetComponent(type);
-                if (component == null)
-                {
-                    component = trans.gameObject.AddComponent(type);
-                }
-                return component;
-            }
-            return null;
+            if (!trans) return false;
+            T t = trans.GetComponent<T>();
+            return t == null;
         }
 
         /// <summary>
@@ -171,7 +78,7 @@ namespace UnityEngine.Library
         /// <returns></returns>
         public static Component Find(this GameObject target, Type type, string path = null)
         {
-            return Find(target.transform, type, path);
+            return target.transform.Find(type, path);
         }
 
         /// <summary>
@@ -184,7 +91,7 @@ namespace UnityEngine.Library
         public static bool ComponentIsNull(this GameObject target, Type type, string path = null)
         {
             var trans = target.transform;
-            if (!path.IsNullOrEmpty())
+            if (!string.IsNullOrEmpty(path))
                 trans = target.transform.Find(path);
             if (!trans) return false;
             var component = trans.GetComponent(type);
@@ -251,86 +158,6 @@ namespace UnityEngine.Library
             behaviour.ToList().ForEach(p => p.enabled = false);
         }
 
-        /// <summary>
-        ///随机位置
-        /// </summary>
-        /// <param name="from"></param>
-        /// <param name="to"></param>
-        public static Vector3 RangeVector3(this Vector3 from, Vector3 to)
-        {
-            return new Vector3(UnityEngine.Random.Range(from.x, to.x), UnityEngine.Random.Range(from.y, to.y),
-                UnityEngine.Random.Range(from.z, to.z));
-        }
-
-        /// <summary>
-        /// 随机位置
-        /// </summary>
-        /// <param name="from"></param>
-        /// <param name="to"></param>
-        public static Vector2 RangeVector2(this Vector2 from, Vector2 to)
-        {
-            return new Vector3(UnityEngine.Random.Range(from.x, to.x), UnityEngine.Random.Range(from.y, to.y));
-        }
-
-        #region GetOrAddComponent
-
-        /// <summary>
-        /// 获取组件(没有时自动添加)
-        /// </summary>
-        /// <param name="go"></param>
-        /// <param name="type"></param>
-        /// <returns></returns>
-        public static Component GetOrAddComponent(this GameObject go, Type type)
-        {
-            if (go == null) return null;
-            Component t = go.GetComponent(type);
-            if (t == null) t = go.AddComponent(type);
-            return t;
-        }
-
-        /// <summary>
-        /// 获取组件(没有时自动添加)
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="component"></param>
-        /// <returns></returns>
-        public static Component GetOrAddComponent(this Component component, Type type)
-        {
-            if (component == null) return null;
-            return component.gameObject.GetOrAddComponent(type);
-        }
-
-        /// <summary>
-        /// 获取组件(没有时自动添加)
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="go"></param>
-        /// <returns></returns>
-        public static T GetOrAddComponent<T>(this GameObject go) where T : Component
-        {
-            if (go == null)
-                return default(T);
-            T t = go.GetComponent<T>();
-            if (t == null) t = go.AddComponent<T>();
-            return t;
-        }
-
-        /// <summary>
-        /// 获取组件(没有时自动添加)
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="component"></param>
-        /// <returns></returns>
-        public static T GetOrAddComponent<T>(this Component component) where T : Component
-        {
-            if (component == null)
-                return default(T);
-            T t = component.GetComponent<T>();
-            if (t == null) t = component.gameObject.AddComponent<T>();
-            return t;
-        }
-
-        #endregion
 
         #region 获取预定义个数的物体或组件
 

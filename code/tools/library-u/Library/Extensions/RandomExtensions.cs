b62db7f42383eb0a336d1272f3598b1566fb9a95
liuxianfeng <1239117205@qq.com>
2019-04-12 10:09:46 +0800

    RandomExtensions.cs

---
+++ code/tools/library-u/Library/Extensions/RandomExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnityEngine.Library
+{
+    /// <summary>
+    /// Random扩展
+    /// </summary>
+    public static class RandomExtensions
+    {
+        public static T RandomValue<T>(this T[] target)
+        {
+            if (target.Length == 0) return default(T);
+            return target[Random.Range(0, target.Length)];
+        }
+
+        public static T RandomValue<T>(this List<T> target)
+        {
+            if (target.Count == 0) return default(T);
+            return target[Random.Range(0, target.Count)];
+        }
+    }
+}
---
+++ code/tools/library/Library/Base/Extensions/RandomExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Library.Extensions.Random
+{
+    /// <summary>
+    /// Random扩展
+    /// </summary>
+    public static class RandomExtensions
+    {
+        public static T RandomValue<T>(this T[] target)
+        {
+            if (target.Length == 0) return default(T);
+            return target[new System.Random().Next(0, target.Length)];
+        }
+
+        public static T RandomValue<T>(this List<T> target)
+        {
+            if (target.Count == 0) return default(T);
+            return target[new System.Random().Next(0, target.Count)];
+        }
+    }
+}

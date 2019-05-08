33041abe0c55c5752a3ee727b46494ec780bb4b5
liuxianfeng <1239117205@qq.com>
2019-05-08 22:16:57 +0800

    protobuf.cs/protobuf.exe

--- code/tools/protobuf/Program.cs
+++ code/tools/protobuf/Program.cs
@@ -14,10 +14,38 @@ namespace protobuf
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            var content = File.ReadAllLines("MessageHandler.txt").ToList();
-            var xx = Directory.GetFiles("meta/msg/", "*.proto").Select(p => Path.GetFileNameWithoutExtension(p).Split('_')).Where(p => p.Length > 1)
-                .Select(p => p[1]).ToList();
-            var list = xx.Select(p => p[0].ToString().ToUpper() + string.Join("", p.Skip(1).Select(q => q.ToString()).ToArray()) + "Mod").ToList();
+            try
+            {
+                GetValue();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+                //exception.StackTrace.Split('\n').ToList().ForEach(Console.WriteLine);
+            }
+            Console.ReadKey();
+        }
+
+        private static void GetValue()
+        {
+            var fileName = Path.GetFullPath("MessageHandler.txt");
+            var content = File.ReadAllLines(fileName).ToList();
+            if (!File.Exists(fileName))
+                throw new Exception(string.Format("[{0}] is not exist!!!", fileName));
+
+            var dirName = Path.GetFullPath("../meta/msg/");
+            if (!Directory.Exists(dirName))
+                throw new Exception(string.Format("[{0}] is not exist!!!", dirName));
+
+            var xx =
+                Directory.GetFiles(dirName, "*.proto")
+                    .Select(p => Path.GetFileNameWithoutExtension(p).Split('_'))
+                    .Where(p => p.Length > 1)
+                    .Select(p => p[1]).ToList();
+            var list =
+                xx.Select(
+                    p => p[0].ToString().ToUpper() + string.Join("", p.Skip(1).Select(q => q.ToString()).ToArray()) + "Mod")
+                    .ToList();
 
             for (int i = content.Count - 1; i >= 0; i--)
             {
@@ -35,7 +63,9 @@ namespace protobuf
                 }
             }
 
-            File.WriteAllLines("MessageHandler.cs", content.ToArray());
+            var newName = Path.ChangeExtension(fileName, ".cs");
+            File.WriteAllLines(newName, content.ToArray());
+            Console.WriteLine("create [" + newName + "] success !!!!!");
         }
     }
 }

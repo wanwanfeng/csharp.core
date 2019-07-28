e5d0e2d80dbe25bf35b3430ac2f9e103538a5ab8
liuxianfeng <1239117205@qq.com>
2019-07-28 15:07:59 +0800

    命令加入单独显示某一组

--- code/tools/excel/Program.cs
+++ code/tools/excel/Program.cs
@@ -14,36 +14,36 @@ namespace Library.Excel
             [Category("两个文件比较"), TypeValue(typeof (CompareJson))] CompareJson,
 
 
-            [Category("Excel文件转换"), Description("Excel->ToXml"), TypeValue(typeof (ActionExcel.ToXml))] ExcelToXml,
-            [Category("Excel文件转换"), Description("Excel->ToCsv"), TypeValue(typeof (ActionExcel.ToCsv))] ExcelToCsv,
-            [Category("Excel文件转换"), Description("Excel->ToJson"), TypeValue(typeof (ActionExcel.ToJson))] ExcelToJson,
-            [Category("Excel文件转换"), Description("Excel->ToExcel"), TypeValue(typeof (ActionExcel.ToExcel))] ExcelToExcel,
-            [Category("Excel文件转换"), Description("Excel->ToOneExcel"), TypeValue(typeof (ActionExcel.ToOneExcel))] ExcelToOneExcel,
+            [Category("Excel文件转换"), Description("Excel->Xml"), TypeValue(typeof (ActionExcel.ToXml))] ExcelToXml,
+            [Category("Excel文件转换"), Description("Excel->Csv"), TypeValue(typeof (ActionExcel.ToCsv))] ExcelToCsv,
+            [Category("Excel文件转换"), Description("Excel->Json"), TypeValue(typeof (ActionExcel.ToJson))] ExcelToJson,
+            [Category("Excel文件转换"), Description("Excel->Excel"), TypeValue(typeof (ActionExcel.ToExcel))] ExcelToExcel,
+            [Category("Excel文件转换"), Description("Excel->OneExcel"), TypeValue(typeof (ActionExcel.ToOneExcel))] ExcelToOneExcel,
 
 
-            [Category("Json文件转换"), Description("Json->ToXml"), TypeValue(typeof (ActionJson.ToXml))] JsonToXml,
-            [Category("Json文件转换"), Description("Json->ToCsv"), TypeValue(typeof (ActionJson.ToCsv))] JsonToCsv,
-            [Category("Json文件转换"), Description("Json->ToExcel"), TypeValue(typeof (ActionJson.ToExcel))] JsonToExcel,
-            [Category("Json文件转换"), Description("Json->ToOneExcel"), TypeValue(typeof (ActionJson.ToOneExcel))] JsonToOneExcel,
+            [Category("Json文件转换"), Description("Json->Xml"), TypeValue(typeof (ActionJson.ToXml))] JsonToXml,
+            [Category("Json文件转换"), Description("Json->Csv"), TypeValue(typeof (ActionJson.ToCsv))] JsonToCsv,
+            [Category("Json文件转换"), Description("Json->Excel"), TypeValue(typeof (ActionJson.ToExcel))] JsonToExcel,
+            [Category("Json文件转换"), Description("Json->OneExcel"), TypeValue(typeof (ActionJson.ToOneExcel))] JsonToOneExcel,
 
-            [Category("Xml文件转换"), Description("Xml->ToCsv"), TypeValue(typeof (ActionXml.ToCsv))] XmlToCsv,
-            [Category("Xml文件转换"), Description("Xml->ToJson"), TypeValue(typeof (ActionXml.ToJson))] XmlToJson,
-            [Category("Xml文件转换"), Description("Xml->ToExcel"), TypeValue(typeof (ActionXml.ToExcel))] XmlToExcel,
-            [Category("Xml文件转换"), Description("Xml->ToOneExcel"), TypeValue(typeof (ActionXml.ToOneExcel))] XmlToOneExcel,
+            [Category("Xml文件转换"), Description("Xml->Csv"), TypeValue(typeof (ActionXml.ToCsv))] XmlToCsv,
+            [Category("Xml文件转换"), Description("Xml->Json"), TypeValue(typeof (ActionXml.ToJson))] XmlToJson,
+            [Category("Xml文件转换"), Description("Xml->Excel"), TypeValue(typeof (ActionXml.ToExcel))] XmlToExcel,
+            [Category("Xml文件转换"), Description("Xml->OneExcel"), TypeValue(typeof (ActionXml.ToOneExcel))] XmlToOneExcel,
 
 
-            [Category("CSV文件转换"), Description("CSV->ToXml"), TypeValue(typeof (ActionCSV.ToXml))] CsvToXml,
-            [Category("CSV文件转换"), Description("CSV->ToJson"), TypeValue(typeof (ActionCSV.ToJson))] CsvToJson,
-            [Category("CSV文件转换"), Description("CSV->ToExcel"), TypeValue(typeof (ActionCSV.ToExcel))] CsvToExcel,
-            [Category("CSV文件转换"), Description("CSV->ToOneExcel"), TypeValue(typeof (ActionCSV.ToOneExcel))] CsvToOneExcel,
+            [Category("CSV文件转换"), Description("CSV->Xml"), TypeValue(typeof (ActionCSV.ToXml))] CsvToXml,
+            [Category("CSV文件转换"), Description("CSV->Json"), TypeValue(typeof (ActionCSV.ToJson))] CsvToJson,
+            [Category("CSV文件转换"), Description("CSV->Excel"), TypeValue(typeof (ActionCSV.ToExcel))] CsvToExcel,
+            [Category("CSV文件转换"), Description("CSV->OneExcel"), TypeValue(typeof (ActionCSV.ToOneExcel))] CsvToOneExcel,
 
-            [Category("文件内容提取与替换"), Description("Excel->ToKvExcel"), TypeValue(typeof (ActionExcel.ToKvExcel))] ExcelToKvExcel,
+            [Category("文件内容提取与替换"), Description("Excel->KvExcel"), TypeValue(typeof (ActionExcel.ToKvExcel))] ExcelToKvExcel,
             [Category("文件内容提取与替换"), Description("Excel->FromKvExcel"), TypeValue(typeof (ActionExcel.KvExcelTo))] ExcelFromKvExcel,
-            [Category("文件内容提取与替换"), Description("Json->ToKvExcel"), TypeValue(typeof (ActionJson.ToKvExcel))] JsonToKvExcel,
+            [Category("文件内容提取与替换"), Description("Json->KvExcel"), TypeValue(typeof (ActionJson.ToKvExcel))] JsonToKvExcel,
             [Category("文件内容提取与替换"), Description("Json->FromKvExcel"), TypeValue(typeof (ActionJson.KvExcelTo))] JsonFromKvExcel,
-            [Category("文件内容提取与替换"), Description("Xml->ToKvExcel"), TypeValue(typeof (ActionXml.ToKvExcel))] XmlToKvExcel,
+            [Category("文件内容提取与替换"), Description("Xml->KvExcel"), TypeValue(typeof (ActionXml.ToKvExcel))] XmlToKvExcel,
             [Category("文件内容提取与替换"), Description("Xml->FromKvExcel"), TypeValue(typeof (ActionXml.KvExcelTo))] XmlFromKvExcel,
-            [Category("文件内容提取与替换"), Description("CSV->ToKvExcel"), TypeValue(typeof (ActionCSV.ToKvExcel))] CsvToKvExcel,
+            [Category("文件内容提取与替换"), Description("CSV->KvExcel"), TypeValue(typeof (ActionCSV.ToKvExcel))] CsvToKvExcel,
             [Category("文件内容提取与替换"), Description("CSV->FromKvExcel"), TypeValue(typeof (ActionCSV.KvExcelTo))] CsvFromKvExcel,
 
 
@@ -66,6 +66,28 @@ namespace Library.Excel
             //Console.ReadKey();
 
             SystemConsole.Run<CaoType>(columnsCount: 4);
+        }
+    }
+
+    public class Json2Excel
+    {
+        public enum CaoType
+        {
+            [Category("文件转换")] [Description("Excel->Json")] [TypeValue(typeof(ActionExcel.ToJson))] ExcelToJson,
+            [Category("文件转换")] [Description("Json->Excel")] [TypeValue(typeof(ActionJson.ToExcel))] JsonToExcel
+        }
+
+        private static void Main(string[] args)
+        {
+            SystemConsole.Run<CaoType>();
+        }
+    }
+
+    public class JsonTools
+    {
+        private static void Main(string[] args)
+        {
+            SystemConsole.Run<Program.CaoType>(group: "Json");
         }
     }
 }

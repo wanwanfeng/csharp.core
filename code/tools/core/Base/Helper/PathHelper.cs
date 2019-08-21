5a0e6452561c03ff332b4d73ef916d9b7fdd20a6
liuxianfeng <liuxianfeng@bilibili.com>
2019-08-21 10:57:31 +0800

    add

---
+++ code/tools/core/Base/Helper/PathHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Library.Extensions;
+
+namespace Library.Helper
+{
+    public partial class PathHelper : DirectoryHelper
+    {
+#if !UNITY
+        public static string GetTempFileName()
+        {
+            return Path.GetTempFileName();
+    }
+#endif
+    }
+}
--- code/tools/library-u/Library/Extensions/LinqExtensions.cs
+++ code/tools/library-u/Library/Extensions/LinqExtensions.cs
@@ -10,68 +10,6 @@ namespace Library
     /// </summary>
     public static partial class LinqExtensions
     {
-        public static Vector2 Sum(this Vector2[] target)
-        {
-            var v = Vector2.zero;
-            foreach (var item in target)
-            {
-                v += item;
-            }
-            return v;
-        }
-
-        public static Vector2 Sum(this List<Vector2> target)
-        {
-            var v = Vector2.zero;
-            foreach (var item in target)
-            {
-                v += item;
-            }
-            return v;
-        }
-
-        public static Vector2 Average(this Vector2[] target)
-        {
-            if (target.Length == 0) return Vector2.zero;
-            return target.Sum() / target.Length;
-        }
-
-        public static Vector2 Average(this List<Vector2> target)
-        {
-            if (target.Count == 0) return Vector2.zero;
-            return target.Sum() / target.Count;
-        }
-
-        public static Vector3 Sum(this Vector3[] target)
-        {
-            var v = Vector3.zero;
-            foreach (var item in target)
-            {
-                v += item;
-            }
-            return v;
-        }
-
-        public static Vector3 Sum(this List<Vector3> target)
-        {
-            var v = Vector3.zero;
-            foreach (var item in target)
-            {
-                v += item;
-            }
-            return v;
-        }
-
-        public static Vector3 Average(this Vector3[] target)
-        {
-            if (target.Length == 0) return Vector3.zero;
-            return target.Sum() / target.Length;
-        }
-
-        public static Vector3 Average(this List<Vector3> target)
-        {
-            if (target.Count == 0) return Vector3.zero;
-            return target.Sum() / target.Count;
-        }
+       
     }
 }

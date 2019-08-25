dff3727db6a952d4c8ba192a2dd8d0257c6ea5fd
liuxianfeng <1239117205@qq.com>
2019-08-26 00:51:01 +0800

    add

---
+++ code/tools/core/Base/Extensions/DelegateExtensions.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Library.Extensions
+{
+    /// <summary>
+    /// Delegate扩展
+    /// </summary>
+    public static partial class DelegateExtensions
+    {
+        public static void Call<T1, T2, T3, T4>(this Action<T1, T2, T3, T4> target, T1 t1, T2 t2, T3 t3, T4 t4)
+        {
+            if (target == null) return;
+            target.Invoke(t1, t2, t3, t4);
+        }
+
+        public static void Call<T1, T2, T3>(this Action<T1, T2, T3> target, T1 t1, T2 t2, T3 t3)
+        {
+            if (target == null) return;
+            target.Invoke(t1, t2, t3);
+        }
+
+        public static void Call<T1, T2>(this Action<T1, T2> target, T1 t1, T2 t2)
+        {
+            if (target == null) return;
+            target.Invoke(t1, t2);
+        }
+
+        public static void Call<T>(this Action<T> target, T t)
+        {
+            if (target == null) return;
+            target.Invoke(t);
+        }
+
+        public static void Call(this Action target)
+        {
+            if (target == null) return;
+            target.Invoke();
+        }
+
+        //----------------------------------------
+
+        public static T Call<T1, T2, T3, T4, T>(this Func<T1, T2, T3, T4, T> target, T1 t1, T2 t2, T3 t3, T4 t4)
+        {
+            if (target == null) return default(T);
+            return target.Invoke(t1, t2, t3, t4);
+        }
+
+        public static T Call<T1, T2, T3, T>(this Func<T1, T2, T3, T> target, T1 t1, T2 t2, T3 t3)
+        {
+            if (target == null) return default(T);
+            return target.Invoke(t1, t2, t3);
+        }
+
+        public static T Call<T1, T2, T>(this Func<T1, T2, T> target, T1 t1, T2 t2)
+        {
+            if (target == null) return default(T);
+            return target.Invoke(t1, t2);
+        }
+
+        public static T Call<T1, T>(this Func<T1, T> target, T1 t1)
+        {
+            if (target == null) return default(T);
+            return target.Invoke(t1);
+        }
+
+        public static T Call<T>(this Func<T> target)
+        {
+            if (target == null) return default(T);
+            return target.Invoke();
+        }
+    }
+}
---
+++ code/tools/library-u/Library/MonoBehaviour/TriggerMonoBehaviour.cs
@@ -0,0 +1,47 @@
+﻿using Library.Extensions;
+using System;
+using UnityEngine;
+
+namespace Library
+{
+    public class TriggerMonoBehaviour : CacheMonoBehaviour
+    {
+        public Action<GameObject, Collision> OnCollisionEnterAction;
+        public Action<GameObject, Collision> OnCollisionStayAction;
+        public Action<GameObject, Collision> OnCollisionExitAction;
+
+        public Action<GameObject, Collider> OnTriggerEnterAction;
+        public Action<GameObject, Collider> OnTriggerStayAction;
+        public Action<GameObject, Collider> OnTriggerExitAction;
+
+        private void OnCollisionEnter(Collision col)
+        {
+            OnCollisionEnterAction.Call(gameObject, col);
+        }
+
+        private void OnCollisionStay(Collision col)
+        {
+            OnCollisionStayAction.Call(gameObject, col);
+        }
+
+        private void OnCollisionExit(Collision col)
+        {
+            OnCollisionExitAction.Call(gameObject, col);
+        }
+
+        private void OnTriggerEnter(Collider col)
+        {
+            OnTriggerEnterAction.Call(gameObject, col);
+        }
+
+        private void OnTriggerStay(Collider col)
+        {
+            OnTriggerStayAction.Call(gameObject, col);
+        }
+
+        private void OnTriggerExit(Collider col)
+        {
+            OnTriggerExitAction.Call(gameObject, col);
+        }
+    }
+}

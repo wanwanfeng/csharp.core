755f17cff6047bb285ee6e1d2c3561b989124fa2
liuxianfeng <liuxianfeng@bilibili.com>
2019-08-27 17:20:07 +0800

    TextureHelper

---
+++ code/tools/library-u/Library/Helper/UnityHelper/AudioClipHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Library.Helper
+{
+    public class AudioClipHelper
+    {
+        public static AudioClip GetRawAudioClip(string path)
+        {
+            return null;
+        }
+
+        //public static AudioClip GetRawTexture(string path)
+        //{
+        //    AudioClip audioClip = new AudioClip();
+        //    try
+        //    {
+        //        byte[] lBytes = System.IO.File.ReadAllBytes(path);
+        //        audioClip.SetData()
+        //        texture.LoadImage(lBytes, false);
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Debug.LogException(e);
+        //        Debug.LogErrorFormat("Fail to load texture [{0}].", path);
+        //        return null;
+        //    }
+        //    return audioClip;
+        //}
+    }
+}

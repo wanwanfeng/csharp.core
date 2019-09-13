d59767fbf686c738f876b9e8920bf1b1c35777db
liuxianfeng <liuxianfeng@bilibili.com>
2019-09-13 16:02:54 +0800

    添加子模块

--- code/tools/core/Base/Extensions/DelegateExtensions.cs
+++ code/tools/core/Base/Extensions/DelegateExtensions.cs
@@ -10,7 +10,7 @@ namespace Library.Extensions
     {
         //----------------------------------------
 
-        public static void Clear<T1, T2, T3, T4>(this Action<T1, T2, T3, T4> target, T1 t1, T2 t2, T3 t3, T4 t4)
+        public static void Clear<T1, T2, T3, T4>(this Action<T1, T2, T3, T4> target)
         {
             if (target == null) return;
             Delegate[] dels = target.GetInvocationList();
@@ -20,7 +20,7 @@ namespace Library.Extensions
             }
         }
 
-        public static void Clear<T1, T2, T3>(this Action<T1, T2, T3> target, T1 t1, T2 t2, T3 t3)
+        public static void Clear<T1, T2, T3>(this Action<T1, T2, T3> target)
         {
             if (target == null) return;
             Delegate[] dels = target.GetInvocationList();
@@ -30,7 +30,7 @@ namespace Library.Extensions
             }
         }
 
-        public static void Clear<T1, T2>(this Action<T1, T2> target, T1 t1, T2 t2)
+        public static void Clear<T1, T2>(this Action<T1, T2> target)
         {
             if (target == null) return;
             Delegate[] dels = target.GetInvocationList();
@@ -40,7 +40,7 @@ namespace Library.Extensions
             }
         }
 
-        public static void Clear<T1>(this Action<T1> target, T1 t1)
+        public static void Clear<T1>(this Action<T1> target)
         {
             if (target == null) return;
             Delegate[] dels = target.GetInvocationList();
@@ -94,7 +94,7 @@ namespace Library.Extensions
 
         //----------------------------------------
 
-        public static void Clear<T1, T2, T3, T4, T>(this Func<T1, T2, T3, T4, T> target, T1 t1, T2 t2, T3 t3, T4 t4)
+        public static void Clear<T1, T2, T3, T4, T>(this Func<T1, T2, T3, T4, T> target)
         {
             if (target == null) return;
             Delegate[] dels = target.GetInvocationList();
@@ -104,7 +104,7 @@ namespace Library.Extensions
             }
         }
 
-        public static void Clear<T1, T2, T3, T>(this Func<T1, T2, T3, T> target, T1 t1, T2 t2, T3 t3)
+        public static void Clear<T1, T2, T3, T>(this Func<T1, T2, T3, T> target)
         {
             if (target == null) return;
             Delegate[] dels = target.GetInvocationList();
@@ -114,7 +114,7 @@ namespace Library.Extensions
             }
         }
 
-        public static void Clear<T1, T2, T>(this Func<T1, T2, T> target, T1 t1, T2 t2)
+        public static void Clear<T1, T2, T>(this Func<T1, T2, T> target)
         {
             if (target == null) return;
             Delegate[] dels = target.GetInvocationList();
@@ -124,7 +124,7 @@ namespace Library.Extensions
             }
         }
 
-        public static void Clear<T1, T>(this Func<T1, T> target, T1 t1)
+        public static void Clear<T1, T>(this Func<T1, T> target)
         {
             if (target == null) return;
             Delegate[] dels = target.GetInvocationList();

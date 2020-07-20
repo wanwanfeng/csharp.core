e3cc841f9bd10b5a7b05f819bbda4b5f24c0ecc6
liuxianfeng <liuxianfeng@bilibili.com>
2020-07-20 10:53:53 +0800

    del

--- code/tools/library-u/Library/StateMachine/BaseState.cs
+++ code/tools/library-u/Library/StateMachine/BaseState.cs
@@ -8,10 +8,6 @@ namespace Library
 {
     public interface ISimpleState<T>
     {
-        void OnEnter(T state);
-        void OnUpdate();
-        void OnExit();
-
         Func<T, bool> IsCanEnter { get; set; }
         Action<T> OnEnterAction { get; set; }
         Action<T> OnUpdateAction { get; set; }
@@ -20,7 +16,6 @@ namespace Library
 
     public class StateMachine<T> : ISimpleState<T>
     {
-
         private float startTime;
 
         public float StartTime
@@ -39,29 +34,12 @@ namespace Library
             if (IsCanEnter != null)
                 isCanEnter = IsCanEnter.Invoke(targetState);
             if (!isCanEnter) return;
-            OnExit();
-            StartTime = Time.time;
-            Args = args;
-            OnEnter(CurState = targetState);
-        }
-
-        public virtual void OnEnter(T state)
-        {
-            Debug.Log(GetType().Name + ":" + state);
-            if (OnEnterAction != null)
-                OnEnterAction.Invoke(state);
-        }
-
-        public virtual void OnExit()
-        {
             if (OnExitAction != null)
                 OnExitAction.Invoke(CurState);
-        }
-
-        public virtual void OnUpdate()
-        {
-            if (OnUpdateAction != null)
-                OnUpdateAction.Invoke(CurState);
+            StartTime = Time.time;
+            Args = args;
+            if (OnEnterAction != null)
+                OnEnterAction.Invoke(CurState = targetState);
         }
 
         public Action<T> OnEnterAction { get; set; }
@@ -73,73 +51,18 @@ namespace Library
     public class StateMachineCache<T> : StateMachine<T>
     {
         protected static Action<T> StateAction = (state) => { };
-        protected Dictionary<T, Action<T>> CacneEnter;
-        protected Dictionary<T, Action<T>> CacneUpdate;
-        protected Dictionary<T, Action<T>> CacneExit;
+        protected Dictionary<T, Action<T>> CacheEnter;
+        protected Dictionary<T, Action<T>> CacheUpdate;
+        protected Dictionary<T, Action<T>> CacheExit;
 
         public virtual void Initialization(object args)
         {
-            CacneEnter = EnumHelper.ToIntDic<T>().ToDictionary(p => p.Key, p => StateAction);
-            CacneUpdate = EnumHelper.ToIntDic<T>().ToDictionary(p => p.Key, p => StateAction);
-            CacneExit = EnumHelper.ToIntDic<T>().ToDictionary(p => p.Key, p => StateAction);
-        }
-
-        public sealed override void OnEnter(T state)
-        {
-            base.OnEnter(state);
-            CacneEnter[state].Invoke(state);
-        }
-
-        public sealed override void OnUpdate()
-        {
-            base.OnUpdate();
-            CacneUpdate[CurState].Invoke(CurState);
-        }
-
-        public sealed override void OnExit()
-        {
-            base.OnExit();
-            CacneExit[CurState].Invoke(CurState);
+            CacheEnter = EnumHelper.ToIntDic<T>().ToDictionary(p => p.Key, p => StateAction);
+            CacheUpdate = EnumHelper.ToIntDic<T>().ToDictionary(p => p.Key, p => StateAction);
+            CacheExit = EnumHelper.ToIntDic<T>().ToDictionary(p => p.Key, p => StateAction);
+            OnEnterAction += (state) => { CacheEnter[state].Invoke(state); };
+            OnUpdateAction += (state) => { CacheUpdate[state].Invoke(state); };
+            OnExitAction += (state) => { CacheExit[state].Invoke(state); };
         }
     }
-
-    #region StateMachineInstanceBase
-
-    /// <summary>
-    /// 状态机管理器管理状态实例（状态实例也是一个状态机）
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <typeparam name="TInstance"></typeparam>
-    public class StateMachineInstance<T, TInstance> : StateMachine<T> where TInstance : StateMachine<T>
-    {
-        public Dictionary<T, TInstance> CacheInstances;
-        public TInstance CurInstance { get; set; }
-
-        public virtual void Initialization()
-        {
-            CacheInstances = new Dictionary<T, TInstance>();
-        }
-
-        public override void OnEnter(T state)
-        {
-            base.OnEnter(state);
-            if (CacheInstances[state] == null) return;
-            CurInstance = CacheInstances[state];
-            CurInstance.OnEnter(state);
-        }
-
-        public override void OnExit()
-        {
-            if (CurInstance == null) return;
-            CurInstance.OnExit();
-        }
-
-        public override void OnUpdate()
-        {
-            if (CurInstance == null) return;
-            CurInstance.OnUpdate();
-        }
-    }
-
-    #endregion
 }

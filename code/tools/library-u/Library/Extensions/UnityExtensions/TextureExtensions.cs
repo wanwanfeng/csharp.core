be08fd1b1a50dc761441ef160ade900dd000bb55
liuxianfeng <liuxianfeng@bilibili.com>
2021-01-21 10:59:18 +0800

    TextureHelper.cs

--- code/tools/library-u/Library/Extensions/UnityExtensions/TextureExtensions.cs
+++ code/tools/library-u/Library/Extensions/UnityExtensions/TextureExtensions.cs
@@ -19,41 +19,72 @@ namespace Library.Extensions
 
         public static void SplitAlpha(this Texture2D texture, string path, string alpha_path)
         {
-            TextureHelper.SplitAlpha(texture, path, alpha_path);
-        }
+			texture.SaveRGB(path);
+			texture.SaveAlpha(alpha_path);
+		}
 
         public static void SaveRGB(this Texture2D texture, string path)
         {
-            TextureHelper.SaveRGB(texture, path);
+			var result =  TextureHelper.SaveRGB(texture);
+			File.WriteAllBytes(path, result.EncodeToJPG(100));
         }
 
         public static void SaveAlpha(this Texture2D texture, string path)
         {
-            TextureHelper.SaveAlpha(texture, path);
-        }
+			var result = TextureHelper.SaveAlpha(texture);
+			File.WriteAllBytes(path, result.EncodeToJPG(100));
+		}
 
         public static void CombineAlpha(this Texture2D texture, string alpha_path, string path)
         {
-            TextureHelper.CombineAlpha(texture, alpha_path, path);
-
+			var result = TextureHelper.CombineAlpha(texture, alpha_path);
+			File.WriteAllBytes(path, result.EncodeToPNG());
         }
 
         public static void CombineAlpha(this Texture2D texture, Texture2D alpha_texture, string path)
         {
-            TextureHelper.CombineAlpha(texture, alpha_texture, path);
-        }
+			var result = TextureHelper.CombineAlpha(texture, alpha_texture);
+			File.WriteAllBytes(path, result.EncodeToPNG());
+		}
 
-        /// <summary>
-        /// 强制2次幂
-        /// </summary>
-        /// <param name="texture"></param>
-        /// <param name="alpha_texture"></param>
-        /// <param name="path"></param>
-        /// <param name="forceOption"></param>
-        public static void ForceSquared(this Texture2D texture, string path, TextAnchor forceOption)
+		/// <summary>
+		/// 强制画布2次幂
+		/// </summary>
+		/// <param name="texture"></param>
+		/// <param name="path"></param>
+		/// <param name="forceOption"></param>
+		public static void ForceSquared(this Texture2D texture, string path, TextAnchor forceOption)
         {
-            TextureHelper.ForceSquared(texture, path, forceOption);
+			var result = TextureHelper.ForceSquared(texture, forceOption);
+			File.WriteAllBytes(path, result.EncodeToPNG());
+		}
 
-        }
-    }
+		/// <summary>
+		/// 画布缩放
+		/// </summary>
+		/// <param name="texture"></param>
+		/// <param name="path"></param>
+		/// <param name="width"></param>
+		/// <param name="height"></param>
+		/// <param name="forceOption"></param>
+		public static void AdjustCanvas(this Texture2D texture, string path, int width, int height, TextAnchor forceOption)
+		{
+			var result = TextureHelper.AdjustCanvas(texture, width, height, forceOption);
+			File.WriteAllBytes(path, result.EncodeToPNG());
+		}
+
+		/// <summary>
+		/// 图像缩放
+		/// </summary>
+		/// <param name="texture"></param>
+		/// <param name="path"></param>
+		/// <param name="width"></param>
+		/// <param name="height"></param>
+		public static void AdjustScale(this Texture2D texture, string path, int width, int height)
+		{
+			var result = TextureHelper.AdjustScale(texture, width, height);
+			File.WriteAllBytes(path, result.EncodeToPNG());
+		}
+
+	}
 }
--- code/tools/library-u/Library/Helper/UnityHelper/TextureHelper.cs
+++ code/tools/library-u/Library/Helper/UnityHelper/TextureHelper.cs
@@ -31,22 +31,16 @@ namespace Library.Helper
             return colors.Any(p => p.a < 1);
         }
 
-        public static void SplitAlpha(Texture2D texture, string path, string alpha_path)
-        {
-            SaveRGB(texture, path);
-            SaveAlpha(texture, alpha_path);
-        }
-
-        public static void SaveRGB(Texture2D texture, string path)
+        public static Texture2D SaveRGB(Texture2D texture)
         {
             var srcPixels = texture.GetPixels();
             Texture2D target = new Texture2D(texture.width, texture.height, TextureFormat.RGB24, false);
             target.SetPixels(srcPixels.Select(p => new Color(p.r, p.g, p.b)).ToArray());
             target.Apply();
-            File.WriteAllBytes(path, target.EncodeToJPG(100));
-        }
+			return target;
+		}
 
-        public static void SaveAlpha(Texture2D texture, string path)
+		public static Texture2D SaveAlpha(Texture2D texture)
         {
             var srcPixels = texture.GetPixels();
             if (!HasAlpha(srcPixels))
@@ -54,16 +48,15 @@ namespace Library.Helper
             Texture2D target = new Texture2D(texture.width, texture.height, TextureFormat.RGB24, false);
             target.SetPixels(srcPixels.Select(p => new Color(p.a, p.a, p.a)).ToArray());
             target.Apply();
-            File.WriteAllBytes(path, target.EncodeToJPG(100));
-        }
+			return target;
+		}
 
-        public static void CombineAlpha(Texture2D texture, string alpha_path, string path)
+		public static Texture2D CombineAlpha(Texture2D texture, string alpha_path)
         {
-            Texture2D alpha_texture = GetRawTexture(alpha_path);
-            CombineAlpha(texture, alpha_texture, path);
+			return CombineAlpha(texture, GetRawTexture(alpha_path));
         }
 
-        public static void CombineAlpha(Texture2D texture, Texture2D alpha_texture, string path)
+        public static Texture2D CombineAlpha(Texture2D texture, Texture2D alpha_texture)
         {
             var alphaPixels = alpha_texture.GetPixels();
             var srcPixels = texture.GetPixels();
@@ -72,54 +65,96 @@ namespace Library.Helper
             Texture2D target = new Texture2D(texture.width, texture.height, TextureFormat.RGBA32, false);
             target.SetPixels(srcPixels.Select((p, i) => new Color(p.r, p.g, p.b, alphaPixels[i].r)).ToArray());
             target.Apply();
-            File.WriteAllBytes(path, target.EncodeToPNG());
-        }
+			return target;
+		}
 
-        /// <summary>
-        /// 强制2次幂
-        /// </summary>
-        /// <param name="alpha_texture"></param>
-        /// <param name="path"></param>
-        public static void ForceSquared(Texture2D texture, string path, TextAnchor forceOption)
-        {
-            int max = Mathf.Max(texture.width, texture.height);
-            int w = Mathf.NextPowerOfTwo(max);
-            var srcPixels = texture.GetPixels();
-            Texture2D target = new Texture2D(w, w, texture.format, false);
-            var color = srcPixels.Select((p, i) => new Color(p.r, p.g, p.b, p.a)).ToArray();
-            switch (forceOption)
-            {
-                default:
-                case TextAnchor.LowerLeft:
-                    target.SetPixels(0, 0, texture.width, texture.height, color);
-                    break;
-                case TextAnchor.LowerCenter:
-                    target.SetPixels((w - texture.width) / 2, 0, texture.width, texture.height, color);
-                    break;
-                case TextAnchor.LowerRight:
-                    target.SetPixels(w - texture.width, 0, texture.width, texture.height, color);
-                    break;
-                case TextAnchor.UpperLeft:
-                    target.SetPixels(0, w - texture.height, texture.width, texture.height, color);
-                    break;
-                case TextAnchor.UpperCenter:
-                    target.SetPixels((w - texture.width) / 2, w - texture.height, texture.width, texture.height, color);
-                    break;
-                case TextAnchor.UpperRight:
-                    target.SetPixels(w - texture.width, w - texture.height, texture.width, texture.height, color);
-                    break;
-                case TextAnchor.MiddleLeft:
-                    target.SetPixels(0, (w - texture.height) / 2, texture.width, texture.height, color);
-                    break;
-                case TextAnchor.MiddleCenter:
-                    target.SetPixels((w - texture.width) / 2, (w - texture.height) / 2, texture.width, texture.height, color);
-                    break;
-                case TextAnchor.MiddleRight:
-                    target.SetPixels(w - texture.width, (w - texture.height) / 2, texture.width, texture.height, color);
-                    break;
-            }
-            target.Apply();
-            File.WriteAllBytes(path, target.EncodeToPNG());
-        }
-    }
+		/// <summary>
+		/// 强制画布2次幂
+		/// </summary>
+		/// <param name="texture"></param>
+		/// <param name="forceOption"></param>
+		/// <returns></returns>
+		public static Texture2D ForceSquared(Texture2D texture, TextAnchor forceOption)
+		{
+			int w = Mathf.NextPowerOfTwo(Mathf.Max(texture.width, texture.height));
+			Texture2D target = AdjustCanvas(texture, w, w, forceOption);
+			target.Apply();
+			return target;
+		}
+
+		/// <summary>
+		/// 画布缩放
+		/// </summary>
+		/// <param name="texture"></param>
+		/// <param name="width"></param>
+		/// <param name="height"></param>
+		/// <param name="forceOption"></param>
+		/// <returns></returns>
+		public static Texture2D AdjustCanvas(Texture2D texture, int width, int height, TextAnchor forceOption)
+		{
+			Texture2D target = new Texture2D(width, height, texture.format, false);
+			var srcPixels = texture.GetPixels();
+			var color = srcPixels.Select((p, i) => new Color(p.r, p.g, p.b, p.a)).ToArray();
+			switch (forceOption)
+			{
+				default:
+				case TextAnchor.LowerLeft:
+					target.SetPixels(0, 0, texture.width, texture.height, color);
+					break;
+				case TextAnchor.LowerCenter:
+					target.SetPixels((width - texture.width) / 2, 0, texture.width, texture.height, color);
+					break;
+				case TextAnchor.LowerRight:
+					target.SetPixels(width - texture.width, 0, texture.width, texture.height, color);
+					break;
+				case TextAnchor.UpperLeft:
+					target.SetPixels(0, height - texture.height, texture.width, texture.height, color);
+					break;
+				case TextAnchor.UpperCenter:
+					target.SetPixels((width - texture.width) / 2, height - texture.height, texture.width, texture.height, color);
+					break;
+				case TextAnchor.UpperRight:
+					target.SetPixels(width - texture.width, height - texture.height, texture.width, texture.height, color);
+					break;
+				case TextAnchor.MiddleLeft:
+					target.SetPixels(0, (height - texture.height) / 2, texture.width, texture.height, color);
+					break;
+				case TextAnchor.MiddleCenter:
+					target.SetPixels((width - texture.width) / 2, (height - texture.height) / 2, texture.width, texture.height, color);
+					break;
+				case TextAnchor.MiddleRight:
+					target.SetPixels(width - texture.width, (height - texture.height) / 2, texture.width, texture.height, color);
+					break;
+			}
+			return target;
+		}
+
+		/// <summary>
+		/// 图像缩放
+		/// </summary>
+		/// <param name="source"></param>
+		/// <param name="width"></param>
+		/// <param name="height"></param>
+		/// <returns></returns>
+		public static Texture2D AdjustScale(Texture2D source, int width, int height)
+		{
+			var newWidth = (float)width;
+			var newHeight = (float)source.width / source.height * height;
+
+			var result = new Texture2D((int)newWidth, (int)newHeight, TextureFormat.RGB24, false);
+
+			Debug.Log(result.width + ":" + result.height);
+
+			for (int i = 0; i < result.width; ++i)
+			{
+				for (int j = 0; j < result.height; ++j)
+				{
+					Color newColor = source.GetPixelBilinear((float)i / result.width, (float)j / result.height);
+					result.SetPixel(i, j, newColor);
+				}
+			}
+			result.Apply();
+			return result;
+		}
+	}
 }

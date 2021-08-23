4b4af2c526a24b07594e4d7b82edc28e2b198ec0
liuxianfeng <liuxianfeng@bilibili.com>
2021-08-23 11:03:51 +0800

    findText配置

--- code/tools/findText/Program.cs
+++ code/tools/findText/Program.cs
@@ -4,12 +4,36 @@ using Library.Extensions;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
+using System.IO;
 
 namespace findText
 {
     internal static class Program
     {
-        public enum ConvertType
+		/// <summary>
+		/// https://blog.csdn.net/Q672405097/article/details/86639488
+		/// </summary>
+		private static Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+		static Program()
+		{
+			Environment.SetEnvironmentVariable("SkipSheet", SystemConsole.GetommandLineArgs("skipSheet"));
+			Environment.SetEnvironmentVariable("TakeSheet", SystemConsole.GetommandLineArgs("takeSheet"));
+
+			Environment.SetEnvironmentVariable("JsonIsIndent", 1.ToString());
+			Environment.SetEnvironmentVariable("KeyLine", 1.ToString());
+			Environment.SetEnvironmentVariable("SkipRows", 0.ToString());
+			Environment.SetEnvironmentVariable("SkipColumns", 0.ToString());
+			Environment.SetEnvironmentVariable("TakeColumns", int.MaxValue.ToString());
+
+			foreach (var item in ConfigurationManager.AppSettings.AllKeys)
+			{
+				Environment.SetEnvironmentVariable(item, config.AppSettings.Settings[item].Value);
+			}
+		}
+
+		public enum ConvertType
         {
             [TypeValue(typeof(ActionForUnity2))] unity,
             [TypeValue(typeof(ActionForCss))] css,

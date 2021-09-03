fc12383d31e207b8c0421807b0850be5c438fc21
liuxianfeng <1239117205@qq..com>
2021-09-03 23:42:10 +0800

    CommandLine.cs

--- code/tools/library-ue/Library/Editor/Common/CommandLine.cs
+++ code/tools/library-ue/Library/Editor/Common/CommandLine.cs
@@ -1,6 +1,7 @@
 ﻿using Library.Helper;
 using System;
-using System.Collections.Generic;
+using System.Collections;
+using System.IO;
 using System.Linq;
 using UnityEngine;
 
@@ -9,80 +10,20 @@ namespace UnityEditor.Library
     [InitializeOnLoad]
     public class CommandLine
     {
-        public static CommandLine Args { get; private set; }
-
         static CommandLine()
         {
-            var lookup = Environment.GetCommandLineArgs()
-                .Where(p => p.StartsWith("--"))
-                .Select(p => p.Split('='))
-                .Select(p => new { k = p.FirstOrDefault().ToLower(), v = p.LastOrDefault() })
-                .GroupBy(p => p.k, p => p.v)
-                .ToDictionary(p => p.Key, p => p.FirstOrDefault());
-
-            Debug.LogFormat("[CommandLine]\n{0}", Args = new CommandLine(lookup));
-        }
-        private Dictionary<string, string> dict = new Dictionary<string, string>();
-        private CommandLine(Dictionary<string, string> lookup)
-        {
-            dict = lookup;
-        }
-        public bool ContainsKey(string key)
-        {
-            return dict.ContainsKey(key);
-        }
-        public string this[string key, string defaultValue = ""]
-        {
-            get
+            foreach (var item in Environment.GetCommandLineArgs().Where(p => p.StartsWith("--")).Select(p => p.Split('=')))
             {
-                if (ContainsKey(key))
-                    return dict[key];
-                return defaultValue;
+                var key = item.FirstOrDefault().TrimStart('-').TrimStart('-');
+                var value = item.LastOrDefault();
+                Environment.SetEnvironmentVariable(key.ToLower(), string.IsNullOrEmpty(value) ? null : value);
             }
-        }
-
-        public int this[string key, int defaultValue = 0]
-        {
-            get
+            if (Environment.GetEnvironmentVariables() is Hashtable hashtable)
             {
-                if (ContainsKey(key))
-                {
-                    var val = 0;
-                    if (int.TryParse(dict[key], out val))
-                    {
-                        defaultValue = val;
-                    }
-                }
-                return defaultValue;
+                var content = JsonHelper.ToJson(hashtable, indentLevel: 2);
+                Debug.LogFormat("[CommandLine]\n{0}", content);
+                File.WriteAllText("CommandLine.json", content);
             }
-        }
-
-        public bool this[string key, bool defaultValue = false]
-        {
-            get
-            {
-                if (ContainsKey(key))
-                {
-                    var val = false;
-                    if (bool.TryParse(dict[key], out val))
-                    {
-                        defaultValue = val;
-                    }
-                }
-                return defaultValue;
-            }
-        }
-
-        public string Check(string key)
-        {
-            if (ContainsKey(key))
-                return dict[key];
-            throw new Exception("this key is not define!");
-        }
-
-        public override string ToString()
-        {
-            return JsonHelper.ToJson(dict, indentLevel: 2);
         }
     }
 }
